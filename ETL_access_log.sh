f08c41b1b3a697cb906f0bfe4ed78c80aeed0f48
Aditya R <dradityarudresh@gmail.com>
2023-10-14 18:06:51 +0530

    V1

---
+++ ETL_access_log.sh
@@ -0,0 +1,24 @@
+
+
+# dowloading the file
+
+echo "Downlaoding Data"
+wget "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Bash%20Scripting/ETL%20using%20shell%20scripting/web-server-access-log.txt.gz"
+
+
+# unzipping the file
+gunzip -f web-server-access-log.txt.gz
+
+# Extracting data
+echo "Extracting Data"
+cut -d"#" -f1-4 web-server-access-log.txt > extracted-data.txt
+
+
+# Transforming data
+echo "Transformation of Data"
+tr "#" "," < extracted-data.txt > transformed-data.csv
+
+# Loading Phase
+echo "Loading Data"
+
+echo "\c ___; \COPY access_log FROM 'transformed-data.csv' DELIMITER ',' CSV HEADER;" | psql --username=___ --host=___
